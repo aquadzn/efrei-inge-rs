f52b36209ddfaf9798b2dd236003722cd8f845bc
aquadzn <williamjcqs8@gmail.com>
2022-11-08 13:56:03 +0100

    systeme

--- S5/Systemes/TP03/ex4.c
+++ S5/Systemes/TP03/ex4.c
@@ -10,7 +10,6 @@ int main() {
     printf("Parent\t\tPID: %d\n", getpid());
 
     if ((fils1 = fork()) == 0) {
-        sleep(1);
         for (int j = 1; j <= 2; j++) {
             if ((petit_fils[j] = fork()) == 0) {
                 printf("Petit-fils %d\tPID: %d\tPPID: %d\n", j, getpid(), getppid());
@@ -22,17 +21,19 @@ int main() {
             wait(NULL);
         }
 
+        sleep(2);
         printf("Fils 1\t\tPID: %d\tPPID: %d\n", getpid(), getppid());
         exit(0);
     }
 
     if ((fils2 = fork()) == 0) {
+        sleep(1);
         printf("Fils 2\t\tPID: %d\tPPID: %d\n", getpid(), getppid());
         exit(0);
     }
 
     if ((fils3 = fork()) == 0) {
-        sleep(2);
+        sleep(3);
         printf("Fils 3\t\tPID: %d\tPPID: %d\n", getpid(), getppid());
         exit(0);
     }
